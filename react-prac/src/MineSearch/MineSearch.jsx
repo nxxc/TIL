f892860787074e3181e3c360a96d0a0b9d080524
nxxc <nomad.nxxc@gmail.com>
2020-06-26 19:32:36 +0900

    Study React Context api 2

--- react-prac/src/MineSearch/MineSearch.jsx
+++ react-prac/src/MineSearch/MineSearch.jsx
@@ -12,10 +12,12 @@ export const CODE = {
   FLAG_MINE: -5,
   CLICKED_MINE: -6,
   OPPEND: 0,
+  halted: false,
 };
 
 export const TableContext = createContext({
   tableData: [],
+  halted: true,
   dispatch: () => {},
 });
 
@@ -57,6 +59,10 @@ const plantMine = (row, column, mine) => {
 
 export const START_GAME = 'START_GAME';
 export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NORMALIZE_CELL = 'NORMALIZE_CELL';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -65,15 +71,143 @@ const reducer = (state, action) => {
       return {
         ...state,
         tableData: plantMine(action.row, action.column, action.mine),
+        halted: false,
       };
     case OPEN_CELL:
       const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      tableData[action.row][action.column] = CODE.OPPEND;
+      tableData.forEach((row, i) => {
+        tableData[i] = [...state.tableData[i]];
+      });
+      const checked = [];
+      const checkArround = (row, column) => {
+        if (
+          [
+            CODE.OPPEND,
+            CODE.FLAG_MINE,
+            CODE.FLAG,
+            CODE.QUESTION,
+            CODE.QUESTION_MINE,
+          ].includes(tableData[row][column])
+        ) {
+          return;
+        }
+        if (
+          row < 0 ||
+          row >= tableData.length ||
+          column < 0 ||
+          column >= tableData[0].length
+        ) {
+          return;
+        }
+        if (checked.includes(row + ',' + column)) {
+          return;
+        } else {
+          checked.push(row + ',' + column);
+        }
+        let arround = [];
+        if (tableData[row - 1]) {
+          arround = arround.concat(
+            tableData[row - 1][column - 1],
+            tableData[row - 1][column],
+            tableData[row - 1][column + 1]
+          );
+        }
+        arround = arround.concat(
+          tableData[row][column - 1],
+          tableData[row][column + 1]
+        );
+        if (tableData[row + 1]) {
+          arround = arround.concat(
+            tableData[row + 1][column - 1],
+            tableData[row + 1][column],
+            tableData[row + 1][column + 1]
+          );
+        }
+        const count = arround.filter((v) =>
+          [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)
+        ).length;
+
+        tableData[row][column] = count;
+        console.log(arround, count);
+        if (count === 0) {
+          const near = [];
+          if (row - 1 > -1) {
+            near.push([row - 1, column - 1]);
+            near.push([row - 1, column]);
+            near.push([row - 1, column + 1]);
+          }
+          near.push([row, column - 1]);
+          near.push([row, column + 1]);
+          if (row + 1 > tableData.length) {
+            near.push([row + 1, column - 1]);
+            near.push([row + 1, column]);
+            near.push([row + 1, column + 1]);
+          }
+          near.forEach((n) => {
+            if (tableData[n[0]][n[1]] !== CODE.OPPEND) {
+              checkArround(n[0], n[1]);
+            }
+          });
+        } else {
+        }
+      };
+      checkArround(action.row, action.column);
       return {
         ...state,
         tableData,
       };
+    case CLICK_MINE: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.column] = CODE.CLICKED_MINE;
+      return {
+        ...state,
+        tableData,
+        halted: true,
+      };
+    }
+    case FLAG_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.column] === CODE.MINE) {
+        tableData[action.row][action.column] = CODE.FLAG_MINE;
+      } else {
+        tableData[action.row][action.column] = CODE.FLAG;
+      }
+      return {
+        ...state,
+        tableData,
+        halted: false,
+      };
+    }
+    case QUESTION_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.column] === CODE.FLAG_MINE) {
+        tableData[action.row][action.column] = CODE.QUESTION_MINE;
+      } else {
+        tableData[action.row][action.column] = CODE.QUESTION;
+      }
+      return {
+        ...state,
+        tableData,
+        halted: false,
+      };
+    }
+    case NORMALIZE_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.column] === CODE.QUESTION_MINE) {
+        tableData[action.row][action.column] = CODE.MINE;
+      } else {
+        tableData[action.row][action.column] = CODE.NORMAL;
+      }
+      return {
+        ...state,
+        tableData,
+        halted: true,
+      };
+    }
     default:
       return state;
   }
@@ -81,19 +215,17 @@ const reducer = (state, action) => {
 
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initiaiState);
-  const value = useMemo(
-    () => ({
-      tableData: state.tableData,
-      dispatch,
-    }),
-    [state.tableData]
-  );
+  const { tableData, halted, timer, result } = state;
+  const value = useMemo(() => ({ tableData, halted, dispatch }), [
+    tableData,
+    halted,
+  ]);
   return (
     <TableContext.Provider value={value}>
       <Form />
-      <div>{state.timer}</div>
+      <div>{timer}</div>
       <Table />
-      <div>{state.result}</div>
+      <div>{result}</div>
     </TableContext.Provider>
   );
 };
