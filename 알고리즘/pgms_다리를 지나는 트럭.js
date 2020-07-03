e38a996b0384a8512857567b6301d657a1e1b5ee
nxxc <nomad.nxxc@gmail.com>
2020-07-03 22:54:16 +0900

    Add remark to algorithm files

--- 알고리즘/pgms_다리를 지나는 트럭.js
+++ 알고리즘/pgms_다리를 지나는 트럭.js
@@ -5,15 +5,17 @@ const truck_weights = [7, 4, 5, 6];
 function solution(bridge_length, weight, truck_weights) {
   let count = 0;
   let bridge_weight = 0;
-  let bridge = Array(bridge_length).fill(0);
+  let bridge = Array(bridge_length).fill(0); //다리 길이만큼 배열 만들기
   let first = truck_weights.shift();
 
-  //     // 처음 1초
-  //   bridge.unshift(first);
-  //   bridge.pop();
-  //   bridge_weight += first;
-  //   count++초
+  // 처음 1초
+  bridge.unshift(first);
+  bridge.pop();
+  bridge_weight += first;
+  count++;
 
+  // 무게 합쳐서 적으면 트럭 빼서 추가하고 한칸씩 이동
+  // 무게 초과하면 0 추가해서 한칸씩 이동
   while (bridge_weight > 0) {
     let bridge_last = bridge.pop();
     bridge_weight -= bridge_last;
@@ -31,3 +33,6 @@ function solution(bridge_length, weight, truck_weights) {
 }
 
 solution(bridge_length, weight, truck_weights);
+
+// 다리를 지나는 트럭 v1
+// 한칸씩 이동으로 해결
--- 알고리즘/pgms_시저암호.js
+++ 알고리즘/pgms_시저암호.js
@@ -6,15 +6,19 @@ function solution(s, n) {
     if (v != ' ') {
       if (asc > 90) {
         asc = ((asc + n - 97) % 26) + 97;
-        answer += String.fromCharCode(asc);
+        answer += String.fromCharCode(asc); // 소문자인 경우
       } else {
         asc = ((asc + n - 65) % 26) + 65;
-        answer += String.fromCharCode(asc);
+        answer += String.fromCharCode(asc); // 대문자인 경우
       }
     } else {
-      answer += ' ';
+      answer += ' '; // 공백문자인 경우
     }
   });
 
   return answer;
 }
+
+// string.charCodeAt(index)  => 인덱스에 있는 문자에 아스키코드 반환
+// String.fromCharCode(ascii) => 아스키코드를 문자로 변경
+// 나머지 연산자 활용하는 방법 알아두기
