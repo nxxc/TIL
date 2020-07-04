aa5fb04c866940b4b88623ce7104b17436b7b261
nxxc <nomad.nxxc@gmail.com>
2020-07-04 23:49:27 +0900

    Coding Test _ make CustomSet v2 (add Class version)

--- 알고리즘/customSet.js
+++ 알고리즘/customSet.js
@@ -1,3 +1,7 @@
+const a = [1, 2, 3, 4, 4, 5, 2, 3];
+const b = [3, 4, 5, 6];
+
+// 함수로
 const removeOverlap = (arr) =>
   arr.reduce((a, c) => (a.includes(c) ? a : [...a, c]), []);
 
@@ -7,3 +11,26 @@ const sumArr = (base, other) =>
 const complement = (base, other) => base.filter((v) => !other.includes(v));
 
 const intersect = (base, other) => base.filter((v) => other.includes(v));
+
+// 클래스
+class customSet {
+  constructor(base, other) {
+    this.base = base.reduce((a, c) => (a.includes(c) ? a : [...a, c]), []);
+    this.other = other.reduce((a, c) => (a.includes(c) ? a : [...a, c]), []);
+  }
+  sumArr = () =>
+    this.other.reduce((a, c) => (a.includes(c) ? a : [...a, c]), this.base);
+
+  complement = () =>
+    [...this.base, ...this.other].filter((v) => !this.other.includes(v));
+
+  intersect = () => this.base.filter((v) => this.other.includes(v));
+}
+
+const se = new customSet(a, b);
+
+console.log(se.base);
+console.log(se.other);
+console.log(se.sumArr());
+console.log(se.complement());
+console.log(se.intersect());
