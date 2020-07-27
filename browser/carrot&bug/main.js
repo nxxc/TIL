c6d1e8e7a5cf8c90f187ea2a892c2cebea423bdc
nxxc <nomad.nxxc@gmail.com>
2020-07-27 20:24:05 +0900

    Browser Bug&Carrot random position

--- browser/carrot&bug/main.js
+++ browser/carrot&bug/main.js
@@ -1,38 +1,42 @@
-const timer = document.querySelector('.timer');
-const counter = document.querySelector('.couter');
-const popup = document.querySelector('popup');
-const bottom = document.querySelector('.bottom');
+const gameBtn = document.querySelector('.game__button');
+const time = document.querySelector('.game__timer');
+const counter = document.querySelector('.game__score');
+const field = document.querySelector('.game__field');
 
-const isPlaying = true; //게임 시작하면 true 정지 false
+const popup = document.querySelector('.pop-up');
+const refreshBtn = document.querySelector('.pop-up__refresh');
+const popupMsg = document.querySelector('.pop-up__message');
 
-//랜덤하게 위치 지정해서 만들기
-//위치는 left: 0~95% top: 0~85%
-function bugsGenerator() {
-  const bug = document.createElement('img');
-  bug.setAttribute('src', './carrot/img/bug.png');
-  bug.style.left = '95%';
-  bug.style.top = '80%';
-  bottom.appendChild(bug);
+//left 0~90
+//top 0~70
+let bugList = [];
+let carrotList = [];
+function imgGenerator() {
+  for (let i = 0; i < 10; i++) {
+    const bug = document.createElement('img');
+    const carrot = document.createElement('img');
+    carrot.src = './carrot/img/carrot.png';
+    bug.setAttribute('class', 'bug');
+    carrot.setAttribute('class', 'carrot');
+    bug.src = './carrot/img/bug.png';
+    bug.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    bug.style.top = `${Math.ceil(Math.random() * 70)}%`;
+    carrot.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    carrot.style.top = `${Math.ceil(Math.random() * 70)}%`;
+    field.appendChild(bug);
+    field.appendChild(carrot);
+    carrotList.push(carrot);
+  }
 }
-bugsGenerator();
-function carrotsGenerator() {}
-
-function randumNumberGenerator() {
-  const left = Array(10)
-    .fill(0)
-    .map((v) => Math.ceil(Math.random() * 96));
-
-  const top = Array(10)
-    .fill(0)
-    .map((v) => Math.ceil(Math.random() * 86));
-  return { left, top };
-}
-console.log(randumNumberGenerator());
-
-//당근이나 벌레 클릭하는 함수
-//이벤트 위임해서 해야할 듯
-function onClick() {}
-
-bottom.addEventListener('click', (e) => {
-  console.log(e.target);
+num = 10;
+field.addEventListener('click', (e) => {
+  if (e.target.classList[0] === 'carrot') {
+    field.removeChild(e.target);
+    num--;
+    counter.innerHTML = num;
+  } else if (e.target.classList[0] === 'bug') {
+    alert('Bye!');
+  }
 });
+
+imgGenerator();
