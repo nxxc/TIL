83517d9ae0b4aa9e1f2f783f7f60e1a898267363
nxxc <nomad.nxxc@gmail.com>
2020-07-30 21:13:17 +0900

    Browser Bug&Carrot refactoring 1

---
+++ browser/carrot&bug/src/field.js
@@ -0,0 +1,53 @@
+'use strict';
+import * as sound from './sound.js';
+
+const carrotSize = 80;
+export default class Field {
+  constructor(carrotCount, bugCount) {
+    this.carrotCount = carrotCount;
+    this.bugCount = bugCount;
+    this.field = document.querySelector('.game__field');
+    this.filedRect = this.field.getBoundingClientRect();
+    this.field.addEventListener('click', this.onClick);
+  }
+
+  setClickListener(onItemClick) {
+    this.onItemClick = onItemClick;
+  }
+  init() {
+    this.field.innerHTML = ``;
+    this._addItem('bug', this.carrotCount, './carrot/img/bug.png');
+    this._addItem('carrot', this.bugCount, './carrot/img/carrot.png');
+  }
+
+  _addItem(className, count, imgPath) {
+    for (let i = 0; i < count; i++) {
+      const img = document.createElement('img');
+      const x1 = 0;
+      const y1 = 0;
+      const x2 = this.filedRect.width - carrotSize;
+      const y2 = this.filedRect.height - carrotSize;
+      img.setAttribute('class', className);
+      img.setAttribute('src', imgPath);
+      img.style.position = 'absolute';
+      img.style.left = `${randomNumber(x1, x2)}px`;
+      img.style.top = `${randomNumber(y1, y2)}px`;
+      this.field.appendChild(img);
+    }
+  }
+
+  onClick = (e) => {
+    const target = e.target;
+    if (target.matches('.carrot')) {
+      target.remove();
+      sound.playCarrot();
+      this.onItemClick && this.onItemClick('carrot');
+    } else if (target.matches('.bug')) {
+      this.onItemClick && this.onItemClick('bug');
+    }
+  };
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
