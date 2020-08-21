0b44bd4efe11e869c293a0f0c927a8e30880bad1
nxxc <nomad.nxxc@gmail.com>
2020-08-21 23:38:23 +0900

    Youtube Clone infinite scroll

--- browser/youtubeclone/dist/main.dev.js
+++ browser/youtubeclone/dist/main.dev.js
@@ -3,7 +3,7 @@
 var url = 'https://yts.mx/api/v2/list_movies.json';
 var contents = document.querySelector('.contents');
 
-function ax() {
+function ax(n) {
   var _ref, data, articles;
 
   return regeneratorRuntime.async(function ax$(_context) {
@@ -11,7 +11,11 @@ function ax() {
       switch (_context.prev = _context.next) {
         case 0:
           _context.next = 2;
-          return regeneratorRuntime.awrap(axios.get(url).then(function (res) {
+          return regeneratorRuntime.awrap(axios.get(url, {
+            params: {
+              page: n
+            }
+          }).then(function (res) {
             return res.data;
           }));
 
@@ -26,11 +30,9 @@ function ax() {
 
         case 6:
           articles = _context.sent;
-          console.log(articles.join(''));
           contents.innerHTML += articles.join('');
-          console.log(data);
 
-        case 10:
+        case 8:
         case "end":
           return _context.stop();
       }
@@ -38,4 +40,22 @@ function ax() {
   });
 }
 
-ax();
+var options = {
+  root: document.querySelector('.contents'),
+  // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
+  rootMargin: '10px',
+  // rootMargin을 '10px 10px 10px 10px'로 설정
+  threshold: [0, 0.5, 1] // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
+
+}; // IntersectionObserver 생성
+
+var io = new IntersectionObserver(function (entries, observer) {
+  // IntersectionObserverEntry 객체 리스트와 observer 본인(self)를 받음
+  // 동작을 원하는 것 작성
+  entries.forEach(function (entry) {
+    // entry와 observer 출력
+    console.log('entry:', entry);
+    console.log('observer:', observer);
+  });
+}, options);
+ax(1);
--- browser/youtubeclone/main.js
+++ browser/youtubeclone/main.js
@@ -2,8 +2,14 @@ const url = 'https://yts.mx/api/v2/list_movies.json';
 
 const contents = document.querySelector('.contents');
 
-async function ax() {
-  const { data } = await axios.get(url).then((res) => res.data);
+async function ax(n) {
+  const { data } = await axios
+    .get(url, {
+      params: {
+        page: n,
+      },
+    })
+    .then((res) => res.data);
   const articles = await data.movies.map((movie) => {
     const article = `
       <article>
@@ -15,9 +21,25 @@ async function ax() {
       `;
     return article;
   });
-  console.log(articles.join(''));
+
   contents.innerHTML += articles.join('');
-  console.log(data);
 }
 
-ax();
+const options = {
+  root: document.querySelector('.contents'), // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
+  rootMargin: '10px', // rootMargin을 '10px 10px 10px 10px'로 설정
+  threshold: [0, 0.5, 1], // 타겟 엘리먼트가 교차영역에 진입했을 때, 교차영역에 타켓 엘리먼트의 50%가 있을 때, 교차 영역에 타켓 엘리먼트의 100%가 있을 때 observe가 반응한다.
+};
+
+// IntersectionObserver 생성
+const io = new IntersectionObserver((entries, observer) => {
+  // IntersectionObserverEntry 객체 리스트와 observer 본인(self)를 받음
+  // 동작을 원하는 것 작성
+  entries.forEach((entry) => {
+    // entry와 observer 출력
+    console.log('entry:', entry);
+    console.log('observer:', observer);
+  });
+}, options);
+
+ax(1);
