829f9edff67a9b3f1f8a4065123fc7640e48d793
nxxc <nomad.nxxc@gmail.com>
2020-09-08 21:18:13 +0900

    [Algorithm]프로그래머스 가사찾기 트라이 자료구조를 사용해서 길이별로 트라이를 만든후에 검색 ?로 시작할 경 우 거꾸로 검색하기위해 반대로 검색하는 트라이도 별도로 만듦

--- 알고리즘/pgms_가사찾기.js
+++ 알고리즘/pgms_가사찾기.js
@@ -28,3 +28,60 @@ console.log(solution(words, queries));
 // 정확성 통과 25점
 // 효율성 탈락 30점
 // 총 55점
+const words = ['frodo', 'front', 'frost', 'frozen', 'frame', 'kakao'];
+const queries = ['fro??', '????o', 'fr???', 'fro???', 'pro?'];
+
+class Trie {
+  constructor() {
+    this.child = {};
+    this.sum = 0;
+  }
+
+  insert(word) {
+    let trie = this;
+    trie.sum += 1;
+    for (const str of word) {
+      if (trie.child[str] === undefined) {
+        trie.child[str] = new Trie();
+      }
+      trie = trie.child[str];
+      trie.sum += 1;
+    }
+  }
+
+  getCount(query) {
+    let trie = this;
+    for (const str of query) {
+      if (str === '?') return trie.sum;
+      if (trie.child[str] === undefined) return 0;
+      trie = trie.child[str];
+    }
+    return 0;
+  }
+}
+
+function solution(words, queries) {
+  const ts = {};
+  const rts = {};
+  let answer = [];
+
+  words.forEach((word) => {
+    const { length } = word;
+    if (ts[length] === undefined) {
+      ts[length] = new Trie();
+      rts[length] = new Trie();
+    }
+    ts[length].insert(word);
+    rts[length].insert([...word].reverse().join(''));
+  });
+
+  answer = queries.map((q) => {
+    const { length } = q;
+    if (ts[length] === undefined) return 0;
+    if (q[0] === '?') return rts[length].getCount([...q].reverse().join(''));
+    return ts[length].getCount(q);
+  });
+  return answer;
+}
+
+// trie 자료구조 이용 갯수마다 다른 트라이 만들고 반대로 검색하는 트라이도 따로 만들어서 풀이
