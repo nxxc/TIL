7bcc266df124ad2251a6c20c5afec00f5e428401
nxxc <nomad.nxxc@gmail.com>
2020-09-11 14:44:39 +0900

    [Algorithm]프로그래머스 수식최대화
    후위표기법 풀이도있지만 splice 함수를 이용해 우선순위별로 3개씩 계산해서
    리턴

---
+++ 알고리즘/pgms_수식 최대화.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+
+function cal(order, num1, num2) {
+  const make = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+  };
+  return make[order](num1, num2);
+}
+function solution(expression) {
+  const priorities = [
+    ['*', '+', '-'],
+    ['*', '-', '+'],
+    ['+', '*', '-'],
+    ['+', '-', '*'],
+    ['-', '*', '+'],
+    ['-', '+', '*'],
+  ];
+  const results = [];
+
+  for (const prioritiy of priorities) {
+    const splitted = expression.split(/([\*\+-])/);
+    for (const order of prioritiy) {
+      while (splitted.includes(order)) {
+        const orderIndex = splitted.indexOf(order);
+        const num1 = splitted[orderIndex - 1];
+        const num2 = splitted[orderIndex + 1];
+        splitted.splice(
+          orderIndex - 1,
+          3,
+          cal(order, Number(num1), Number(num2))
+        );
+      }
+    }
+    results.push(Math.abs(splitted[0]));
+  }
+
+  return Math.max(...results);
+}
