98d5eeeaf0b4c8499188d932686a8b6e5d0b45e6
nxxc <nomad.nxxc@gmail.com>
2020-11-02 22:38:00 +0900

    [algorithm] 삼각달팽이이

--- 알고리즘/pgms_삼각달팽이.js
+++ 알고리즘/pgms_삼각달팽이.js
@@ -1,44 +1,29 @@
 function solution(n) {
-  var answer = [];
-  const map = Array(n)
-    .fill(0)
-    .map((v, i) => Array(i + 1).fill(0));
-  const maxNum = (n * (n + 1)) / 2;
-  let top = 0;
-  let left = 0;
-  let right = n;
-  let bottom = n;
-  let state = 0;
-  let num = 0;
-  while (num <= maxNum) {
-    if (state === 0) {
-      for (let i = top; i < bottom; i++) {
-        map[i][left] = num++;
-      }
-      left++;
-      top++;
-      state = 1;
-      if (state === 1) {
-        for (let i = left; i < right; i++) {
-          map[bottom - 1][i] = num++;
+    var answer = [];
+    let num = 1;
+    const map = Array.from(Array(n), (_, i) => Array(i + 1).fill(0));
+    let top = 0;
+    let left = 0;
+    let right = n - 1;
+    let bottom = n - 1;
+    let counter = 0;
+    while (top <= bottom && left <= right) {
+        for (let i = top; i <= bottom; i++) {
+            map[i][left] = num++;
+        }
+        top++;
+        left++;
+        for (let i = left; i <= right; i++) {
+            map[bottom][i] = num++;
         }
         bottom--;
         right--;
-        state = 2;
-      }
-
-      if (state === 2) {
-        console.log(state);
-        console.log(bottom);
-        for (let i = bottom; i < top; i--) {
-          console.log(i, right);
-          map[i][map[i].length - 1] = num++;
+        for (let i = bottom; i >= top; i--) {
+            map[i][map[i].length - 1 - counter] = num++;
         }
-        state = 0;
-      }
+        top++;
+        right--;
+        counter++;
     }
-    break;
-  }
-  console.log(map);
-  return answer;
+    return map.flat();
 }
